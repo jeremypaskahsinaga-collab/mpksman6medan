f56fb03376b0a4cc942753d895822e39b82de599
varka <jeremypaskahsinaga@gmail.com>
2026-07-08 10:22:40 +0700

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,9 @@
+
+setTimeout(()=>{
+document.getElementById('s1').classList.remove('active');
+document.getElementById('s2').classList.add('active');
+},2200);
+function next(){
+document.body.innerHTML='<div style="display:flex;height:100vh;justify-content:center;align-items:center;background:#111;color:#fff;font-family:Arial"><div><h2>Mengoptimalkan tampilan...</h2></div></div>';
+setTimeout(()=>location.href='index.html',1800);
+}
