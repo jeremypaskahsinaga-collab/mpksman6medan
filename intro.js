007f9165757cf0a887d2c30cf6586015cbcce6d7
varka <jeremypaskahsinaga@gmail.com>
2026-07-08 10:31:54 +0700

    Update intro.js

--- intro.js
+++ intro.js
@@ -5,5 +5,5 @@ document.getElementById('s2').classList.add('active');
 },2200);
 function next(){
 document.body.innerHTML='<div style="display:flex;height:100vh;justify-content:center;align-items:center;background:#111;color:#fff;font-family:Arial"><div><h2>Mengoptimalkan tampilan...</h2></div></div>';
-setTimeout(()=>location.href='index.html',1800);
+setTimeout(()=>location.href='home.html',1800);
 }
